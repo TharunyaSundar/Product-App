532ed5f73f42a6db32ffb806600e4096fa7fb7c2
TharunyaSundar <tsundar@binghamton.edu>
2025-03-25 16:09:14 -0400

    final

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js"
@@ -9,14 +10,25 @@ dotenv.config();
 const app = express();
 //listen on a port and call the callback function once ready
 
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve() 
 //middleware to allow parse request of the body, ie, accept JSON data in the req.body
 app.use(express.json()); //middelware is a function that runs b4 you send a response back to the client
 
 app.use("/api/products",productRoutes)
 
-app.listen(PORT,() =>{
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (res, req)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
+
+app.listen(4000,() =>{
     connectDB();
     console.log("server started at http://localhost:"+PORT);
 })
